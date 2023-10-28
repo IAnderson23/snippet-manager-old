d304d7d452642c562cf195d6688ad7c13155b89e
Isaiah Anderson <isaiahanderson77@icloud.com>
2023-10-28 11:12:08 -0400

    initialize a constructor for directory target object

---
+++ dev-diary/src/Utility/Constructors/directory-constructor.utility.js
@@ -0,0 +1,15 @@
+
+const directoryConstructor = {
+  default: {type: "smartGroup", target: "all"},
+  folder: (target) => {
+    return {type: "folder", target: target}
+  },
+  tag: (target) => {
+    return {type: "tag", target: target}
+  },
+  smartGroup: (target) => {
+    return {type: "smartGroup", target: target}
+  }
+}
+
+export default directoryConstructor;
