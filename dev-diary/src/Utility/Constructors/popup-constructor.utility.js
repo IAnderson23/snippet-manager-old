f04f2f831f247f027414b2fd1f6a5558090d4cc0
Isaiah Anderson <isaiahanderson77@icloud.com>
2023-10-28 11:11:55 -0400

    initialize a constructor for popup target object

---
+++ dev-diary/src/Utility/Constructors/popup-constructor.utility.js
@@ -0,0 +1,40 @@
+import {folderSchema, fragmentSchema, snippetSchema} from "../../db";
+
+const popupConstructor = {
+  default: {type: "", subType: "", target: {}},
+  create: {
+    folder: () => {
+      return {type: "create", subType: "folder", target: folderSchema}
+    },
+    snippet: () => {
+      return {type: "create", subType: "snippet", target: snippetSchema}
+    },
+    fragment: () => {
+      return {type: "create", subType: "fragment", target: fragmentSchema}
+    },
+  },
+  edit: {
+    folder: (target) => {
+      return {type: "edit", subType: "folder", target: target}
+    },
+    snippet: (target) => {
+      return {type: "edit", subType: "snippet", target: target}
+    },
+    fragment: (target) => {
+      return {type: "edit", subType: "fragment", target: target}
+    },
+  },
+  delete: {
+    folder: (target) => {
+      return {type: "delete", subType: "folder", target: target}
+    },
+    snippet: (target) => {
+      return {type: "delete", subType: "snippet", target: target}
+    },
+    fragment: (target) => {
+      return {type: "delete", subType: "fragment", target: target}
+    },
+  },
+}
+
+export default popupConstructor;
