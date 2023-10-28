3a185419725cd903e83593018a13a5d6b6653ef5
Isaiah Anderson <isaiahanderson77@icloud.com>
2023-10-28 00:16:42 -0400

    initialize atoms

---
+++ dev-diary/src/atom.js
@@ -0,0 +1,20 @@
+import {atom} from "jotai";
+
+export const directoryAtom = atom({});
+
+export const folderAtom = atom({})
+export const allFoldersAtom = atom([]);
+
+export const snippetAtom = atom({});
+export const snippetIDAtom = atom(0);
+export const allSnippetsAtom = atom([]);
+
+export const fragmentAtom = atom({});
+export const fragmentIDAtom = atom(0);
+export const allFragmentsAtom = atom([]);
+
+export const userQueryAtom = atom("");
+
+export const popupAtom = atom({});
+
+export const cmPositionAtom = atom({});
