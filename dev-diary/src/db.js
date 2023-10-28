2cf92ef43b1246c655b4e248071ee19e7fc446ff
Isaiah Anderson <isaiahanderson77@icloud.com>
2023-10-28 00:07:15 -0400

    updated DB stores and added store schemas

--- dev-diary/src/db.js
+++ dev-diary/src/db.js
@@ -3,7 +3,29 @@ import Dexie from "dexie";
 export const db = new Dexie("dev-diary-db");
 
 db.version(1).stores({
-    folder: "++id, name",
-    snippet: "++id, folderID, name, *tags",
-    fragment: "++id, snippetID, name"
+    folders: "++id, &name, order, isFavorite",
+    snippets: "++id, folderID, name, *tags, isFavorite, lastViewed",
+    fragments: "++id, snippetID, name, code, language, order"
 })
+
+export const folderSchema = {
+    name: "",
+    order: 0,
+    isFavorite: false,
+}
+
+export const snippetSchema = {
+    folderID: 0,
+    name: "",
+    tags: [],
+    isFavorite: false,
+    lastViewed: new Date()
+}
+
+export const fragmentSchema = {
+    snippetID: 0,
+    name: "Fragment",
+    code: 'console.log("Hello World");',
+    language: "Javascript",
+    order: 0,
+}
