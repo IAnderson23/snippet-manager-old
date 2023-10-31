7975860baba5ccfc253ee2dac2593fc978836b4e
Isaiah Anderson <isaiahanderson77@icloud.com>
2023-10-31 09:15:48 -0400

    change classname to id

--- dev-diary/src/App.js
+++ dev-diary/src/App.js
@@ -1,10 +1,11 @@
 import DirectorySelector from './Layout/Directory Selector/directory-selector.layout';
 import SnippetSelector from './Layout/Snippet Selector/snippet-selector.layout';
 import CodeEditor from './Layout/Code Editor/code-editor.layout';
+import "./Sass/app.sass"
 
 function App() {
   return (
-    <div className="app">
+    <div id="app">
       <DirectorySelector />
       <SnippetSelector />
       <CodeEditor />
