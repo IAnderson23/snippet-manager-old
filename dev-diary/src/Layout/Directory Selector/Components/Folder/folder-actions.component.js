71c0993f553d18195ec5a60aa007eb4407922c19
Isaiah Anderson <isaiahanderson77@icloud.com>
2023-10-31 09:18:54 -0400

    created actions for folder list items

--- dev-diary/src/Layout/Directory Selector/Components/Folder/folder-actions.component.js
+++ dev-diary/src/Layout/Directory Selector/Components/Folder/folder-actions.component.js
@@ -1,6 +1,17 @@
+import {useSetAtom} from "jotai";
+import {popupAtom} from "../../../../Atoms/popup.atom";
+import {popupConstructor} from "../../../../Utility/Constructors/popup-constructor.utility";
+import {editIcon, trashIcon} from "../../../../Assets/svg-icons.asset";
 
-function FolderActions() {
+function FolderActions({popupTarget}) {
+  const setPopup = useSetAtom(popupAtom);
 
+  return (
+    <div className={"item-actions"}>
+      <button onClick={() => setPopup(popupConstructor.edit.folder(popupTarget))}>{editIcon}</button>
+      <button onClick={() => setPopup(popupConstructor.delete.folder(popupTarget))}>{trashIcon}</button>
+    </div>
+  )
 }
 
 export default FolderActions;
