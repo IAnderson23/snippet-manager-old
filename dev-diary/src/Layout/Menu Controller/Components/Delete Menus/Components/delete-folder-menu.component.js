d55a235cc6fcc01349fe0a7dfeb1ac7095485b99
Isaiah Anderson <isaiahanderson77@icloud.com>
2023-11-17 10:45:22 -0500

    feat: Add DeleteFolderMenu component
    Introduce a new React component, DeleteFolderMenu, for confirming the deletion of a folder.
    Utilizes Jotai to manage the directory and menu states with directoryAtom and menuAtom.
    Includes functions for handling folder deletion, updating the directory state, and resetting the menu state.

---
+++ dev-diary/src/Layout/Menu Controller/Components/Delete Menus/Components/delete-folder-menu.component.js
@@ -0,0 +1,35 @@
+import {isEqual} from "lodash";
+import {useAtom, useAtomValue, useSetAtom} from "jotai";
+
+import {directoryConstructor} from "../../../../../Utility/Constructors/directory-constructor.utility";
+import {menuConstructor} from "../../../../../Utility/Constructors/menu-constructor.utility";
+import {directoryAtom} from "../../../../../Atoms/Directory/directory.atom";
+import {deleteFolder} from "../../../../../Database/CRUD/folder.crud";
+import {menuAtom} from "../../../../../Atoms/menu-atom";
+
+function DeleteFolderMenu() {
+  const [directory, setDirectory] = useAtom(directoryAtom);
+  const setMenu = useSetAtom(menuAtom);
+  const {target: folder} = useAtomValue(menuAtom);
+
+  function deleteHandler() {
+    deleteFolder(folder.id).then(res => console.log(res))
+    if (isEqual(directory, {type: "folder", target: folder.id}))
+      setDirectory(directoryConstructor.default);
+    setMenu(menuConstructor.default);
+  }
+
+  function cancelHandler() {
+    setMenu(menuConstructor.default);
+  }
+
+  return (
+    <div className={"menu"}>
+      <h2>Are you sure you want to permanently remove {folder.name}?</h2>
+      <button className={"submit"} type={"button"} onClick={deleteHandler}>Delete</button>
+      <button className={"cancel"} type={"button"} onClick={cancelHandler}>Cancel</button>
+    </div>
+  )
+}
+
+export default DeleteFolderMenu;
