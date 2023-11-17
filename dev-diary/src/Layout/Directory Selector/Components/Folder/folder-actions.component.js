157c0322c14c4bf04e6f1ab6ce912a11800b337f
Isaiah Anderson <isaiahanderson77@icloud.com>
2023-11-17 10:21:46 -0500

    refactor: Update FolderActions to use menuAtom
    Modify FolderActions component to use menuAtom for managing the application's menu state.
    Replace the usage of popupAtom and popupConstructor with menuAtom and menuConstructor

--- dev-diary/src/Layout/Directory Selector/Components/Folder/folder-actions.component.js
+++ dev-diary/src/Layout/Directory Selector/Components/Folder/folder-actions.component.js
@@ -1,15 +1,15 @@
 import {useSetAtom} from "jotai";
-import {popupAtom} from "../../../../Atoms/popup.atom";
-import {popupConstructor} from "../../../../Utility/Constructors/popup-constructor.utility";
+import {menuAtom} from "../../../../Atoms/menu-atom";
+import {menuConstructor} from "../../../../Utility/Constructors/menu-constructor.utility";
 import {editIcon, trashIcon} from "../../../../Assets/svg-icons.asset";
 
-function FolderActions({popupTarget}) {
-  const setPopup = useSetAtom(popupAtom);
+function FolderActions({menuTarget}) {
+  const setMenu = useSetAtom(menuAtom);
 
   return (
     <div className={"item-actions"}>
-      <button onClick={() => setPopup(popupConstructor.edit.folder(popupTarget))}>{editIcon}</button>
-      <button onClick={() => setPopup(popupConstructor.delete.folder(popupTarget))}>{trashIcon}</button>
+      <button onClick={() => setMenu(menuConstructor.edit.folder(menuTarget))}>{editIcon}</button>
+      <button onClick={() => setMenu(menuConstructor.delete.folder(menuTarget))}>{trashIcon}</button>
     </div>
   )
 }
