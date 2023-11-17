79aaa6b75fb7f2c222ca17ec3bbb17c2144e9d1e
Isaiah Anderson <isaiahanderson77@icloud.com>
2023-11-17 09:14:30 -0500

    feat: Introduce SnippetSelector component
    Create a new React component, SnippetSelector, that serves as a container for managing snippets.
    The component includes a header with a SnippetSearchBar and an AddSnippetButton.
    It also embeds the SnippetList component for displaying the list of snippets.

--- dev-diary/src/Layout/Snippet Selector/snippet-selector.layout.js
+++ dev-diary/src/Layout/Snippet Selector/snippet-selector.layout.js
@@ -1,6 +1,17 @@
+import SnippetSearchBar from "./Components/snippet-search-bar.component";
+import AddSnippetButton from "./Components/add-snippet-button.component";
+import SnippetList from "./Components/snippet-list.component";
 
 function SnippetSelector() {
-    
+    return (
+      <div id={"snippet-selector"}>
+        <div id={"snippet-selector-header"}>
+          <SnippetSearchBar />
+          <AddSnippetButton />
+        </div>
+        <SnippetList />
+      </div>
+    )
 }
 
 export default SnippetSelector;
