8087575cc70c269095552a93a7a85004b4e23fae
Isaiah Anderson <isaiahanderson77@icloud.com>
2023-11-17 11:24:50 -0500

    refactor: improve useSnippetFragments hook
    Refactor the useSnippetFragments hook for better readability and consistency.
    Move the import statements to align with the typical order, and update the createFragment call to remove unnecessary logging.

--- dev-diary/src/Custom Hooks/use-snippet-fragments.hook.js
+++ dev-diary/src/Custom Hooks/use-snippet-fragments.hook.js
@@ -2,9 +2,9 @@ import {useAtomValue} from "jotai";
 import {useEffect, useState} from "react";
 
 import {snippetIDAtom} from "../Atoms/Snippet/snippet-id.atom";
-import useAllFragments from "./use-all-fragments.hook";
 import {createFragment} from "../Database/CRUD/fragment.crud";
 import {fragmentSchema} from "../Database/database-schema";
+import useAllFragments from "./use-all-fragments.hook";
 
 function useSnippetFragments() {
   const snippetID = useAtomValue(snippetIDAtom);
@@ -15,11 +15,12 @@ function useSnippetFragments() {
   useEffect(() => {
     if (fragments.isLoaded) {
       const filteredFragments = fragments.data.filter(fragment => fragment.snippetID === snippetID)
+
       if (filteredFragments.length) {
         setSnippetFragments(filteredFragments)
         setIsLoaded(true)
       } else {
-        createFragment({...fragmentSchema, snippetID: snippetID}).then(res => console.log(res))
+        createFragment({...fragmentSchema, snippetID: snippetID});
       }
     }
   }, [snippetID, fragments.isLoaded, fragments.data])
