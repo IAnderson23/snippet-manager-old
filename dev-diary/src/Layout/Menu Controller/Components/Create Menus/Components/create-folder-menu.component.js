fe5ae83d66b85b278db48618fa6dc4702197d2d6
Isaiah Anderson <isaiahanderson77@icloud.com>
2023-11-17 10:50:47 -0500

    feat: Add CreateFolderMenu component
    Introduce a new React component, CreateFolderMenu, responsible for rendering a form to create new folders.
    Uses Jotai for state management with the menuAtom.
    Includes a submitHandler function to handle form submission and create a new folder using CRUD operations.

---
+++ dev-diary/src/Layout/Menu Controller/Components/Create Menus/Components/create-folder-menu.component.js
@@ -0,0 +1,28 @@
+import {useAtomValue, useSetAtom} from "jotai";
+
+import {menuConstructor} from "../../../../../Utility/Constructors/menu-constructor.utility";
+import {createFolder} from "../../../../../Database/CRUD/folder.crud";
+import FolderForm from "../../Forms/folder-form.component";
+import {menuAtom} from "../../../../../Atoms/menu-atom";
+
+function CreateFolderMenu() {
+  const setMenu = useSetAtom(menuAtom);
+  const {target: folder} = useAtomValue(menuAtom);
+
+  function submitHandler(e, folder) {
+    e.preventDefault();
+    createFolder(folder).then(res => console.log(res));
+    setMenu(menuConstructor.default);
+  }
+
+  return (
+    <div className={"menu"}>
+      <h3>Create Folder</h3>
+      <FolderForm initialFolder={folder}
+                  submitHandler={submitHandler}
+                  menuType={"Create"}/>
+    </div>
+  )
+}
+
+export default CreateFolderMenu;
