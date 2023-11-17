97186ea589accee545002e78435b4877b861b9c1
Isaiah Anderson <isaiahanderson77@icloud.com>
2023-11-17 09:13:16 -0500

    feat: Add SnippetTags component
    Introduce a new React component, SnippetTags, that utilizes Jotai for state management.
    The component displays tags associated with a snippet fetched from the snippetAtom.
    If the snippet has tags, they are rendered in an unordered list within the tag-list-container.

---
+++ dev-diary/src/Layout/Code Editor/Components/Header/Components/snippet-tags.component.js
@@ -0,0 +1,19 @@
+import {useAtomValue} from "jotai";
+import {snippetAtom} from "../../../../../Atoms/Snippet/snippet.atom";
+import {isEmpty} from "lodash";
+
+function SnippetTags() {
+  const snippet = useAtomValue(snippetAtom);
+
+  return (
+    <div id={"tag-list-container"}>
+      {isEmpty(snippet.tags) ? null :
+        <ul id={"tag-list"}>
+          {snippet.tags.map((tag, i) => <li key={i}>{tag}</li>)}
+        </ul>
+      }
+    </div>
+  )
+}
+
+export default SnippetTags;
