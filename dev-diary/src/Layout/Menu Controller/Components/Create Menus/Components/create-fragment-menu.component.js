fd1da306d9979c03512d06f38712b9e1f8c2daac
Isaiah Anderson <isaiahanderson77@icloud.com>
2023-11-17 10:51:16 -0500

    feat: Add CreateFragmentMenu component
    Introduce a new React component, CreateFragmentMenu, responsible for rendering a form to create new fragments.
    Uses Jotai for state management with the menuAtom and snippetIDAtom.
    Includes a submitHandler function to handle form submission and create a new fragment using CRUD operations.

---
+++ dev-diary/src/Layout/Menu Controller/Components/Create Menus/Components/create-fragment-menu.component.js
@@ -0,0 +1,30 @@
+import {useAtomValue, useSetAtom} from "jotai";
+import {menuAtom} from "../../../../../Atoms/menu-atom";
+import {snippetIDAtom} from "../../../../../Atoms/Snippet/snippet-id.atom";
+import {fragmentSchema} from "../../../../../Database/database-schema";
+import {createFragment} from "../../../../../Database/CRUD/fragment.crud";
+import {menuConstructor} from "../../../../../Utility/Constructors/menu-constructor.utility";
+import FragmentForm from "../../Forms/fragment-form.component";
+
+function CreateFragmentMenu() {
+  const setMenu = useSetAtom(menuAtom);
+  const snippetID = useAtomValue(snippetIDAtom);
+  const fragment = {...fragmentSchema, snippetID};
+
+  function submitHandler(e, fragment) {
+    e.preventDefault();
+    createFragment({...fragment, name: fragment.name});
+    setMenu(menuConstructor.default);
+  }
+
+  return (
+    <div className={"menu"}>
+      <h3>Create Fragment</h3>
+      <FragmentForm initialFragment={fragment}
+                    submitHandler={submitHandler}
+                    menuType={"Create"}/>
+    </div>
+  )
+}
+
+export default CreateFragmentMenu;
