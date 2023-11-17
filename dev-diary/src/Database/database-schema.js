a543fc9f06309fa713bea1268a0705f0092b601e
Isaiah Anderson <isaiahanderson77@icloud.com>
2023-11-17 11:21:30 -0500

    refactor: Update folder, snippet, and fragment schemas
    Refactor the folder, snippet, and fragment schemas to include an 'id' field for improved consistency.
    Ensure each schema now explicitly defines an 'id' property, making it easier to manage and identify individual items.

--- dev-diary/src/Database/database-schema.js
+++ dev-diary/src/Database/database-schema.js
@@ -1,11 +1,13 @@
 
 export const folderSchema = {
+  id: undefined,
   name: "",
   order: 0,
   isFavorite: false,
 }
 
 export const snippetSchema = {
+  id: undefined,
   folderID: 0,
   name: "",
   tags: [],
@@ -14,6 +16,7 @@ export const snippetSchema = {
 }
 
 export const fragmentSchema = {
+  id: undefined,
   snippetID: 0,
   name: "Fragment",
   code: 'console.log("Hello World");',
