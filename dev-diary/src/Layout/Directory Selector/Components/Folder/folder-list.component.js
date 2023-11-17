af9a375e11a4291c44315d178742f2cebd6df5e2
Isaiah Anderson <isaiahanderson77@icloud.com>
2023-11-17 10:24:14 -0500

    refactor: Update FolderList to use menuAtom and FolderItem
    Refactor FolderList component to use menuAtom for managing the application's menu state.
    Replace the usage of directoryAtom, popupAtom, popupConstructor, and directoryConstructor with menuAtom and menuConstructor.
    Introduce FolderItem component to represent individual folder items, promoting modularity and reusability.

--- dev-diary/src/Layout/Directory Selector/Components/Folder/folder-list.component.js
+++ dev-diary/src/Layout/Directory Selector/Components/Folder/folder-list.component.js
@@ -1,29 +1,17 @@
-import {useAtom, useSetAtom} from "jotai";
-import {directoryAtom} from "../../../../Atoms/Directory/directory.atom";
-import {popupAtom} from "../../../../Atoms/popup.atom";
-import {popupConstructor} from "../../../../Utility/Constructors/popup-constructor.utility";
-import {directoryConstructor} from "../../../../Utility/Constructors/directory-constructor.utility";
-import {addIcon, folderIcon} from "../../../../Assets/svg-icons.asset";
-import FolderActions from "./folder-actions.component";
-import useFolderList from "../../../../Custom Hooks/use-folder-list.hook";
+import {useSetAtom} from "jotai";
+
+import {menuConstructor} from "../../../../Utility/Constructors/menu-constructor.utility";
+import useAllFolders from "../../../../Custom Hooks/use-all-folders.hook";
+import {addIcon} from "../../../../Assets/svg-icons.asset";
+import {menuAtom} from "../../../../Atoms/menu-atom";
+import FolderItem from "./folder-item.component";
 
 function FolderList() {
-  const [directory, setDirectory] = useAtom(directoryAtom)
-  const setPopup = useSetAtom(popupAtom);
-  const folderList = useFolderList();
-
-  console.log(folderList)
-
-  function activeChecker(target) {
-    return directory.type === "folder" && directory.target === target ? "active" : "";
-  }
-
-  function onClickHandler(target) {
-    setDirectory(directoryConstructor.folder(target))
-  }
+  const setMenu = useSetAtom(menuAtom);
+  const allFolders = useAllFolders();
 
   function onAddHandler() {
-    setPopup(popupConstructor.create.folder())
+    setMenu(menuConstructor.create.folder())
   }
 
   return (
@@ -33,15 +21,7 @@ function FolderList() {
         <button onClick={onAddHandler}>{addIcon}</button>
       </div>
       <ul>
-        {folderList?.map(folder => {
-          return  <li key={folder.id} className={"directory-item " + activeChecker(folder.id)}>
-            <div className={"item-header"} onClick={() => onClickHandler(folder.id)}>
-              {folderIcon}
-              <p className={"item-name"}>{folder.name}</p>
-            </div>
-            <FolderActions popupTarget={folder}/>
-          </li>
-        })}
+        {allFolders.map((folder, i) =><FolderItem folder={folder} key={i} />)}
       </ul>
     </div>
   )
