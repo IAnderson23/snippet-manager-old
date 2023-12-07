a028de84f5f55826e547ff80233b9a35c2fa004a
Isaiah Anderson <isaiahanderson77@icloud.com>
2023-12-07 09:17:07 -0500

    refactor: rename and refactor
    rename FolderActions component to FolderActionsDropdown in FolderItem
    add classname to item icon

--- src/Layout/Directory Selector/Components/Folder/folder-item.component.js
+++ src/Layout/Directory Selector/Components/Folder/folder-item.component.js
@@ -5,7 +5,7 @@ import { mdiFolderOpenOutline } from '@mdi/js';
 
 import {directoryConstructor} from "../../../../Utility/Constructors/directory-constructor.utility";
 import {directoryAtom} from "../../../../Atoms/Directory/directory.atom";
-import FolderActions from "./folder-actions.component";
+import FolderActionsDropdown from "./folder-actions.component";
 
 function FolderItem({folder}) {
   const [directory, setDirectory] = useAtom(directoryAtom)
@@ -21,10 +21,10 @@ function FolderItem({folder}) {
   return (
     <li key={folder.id} className={"directory-item " + activeChecker(folder.id)}>
       <div className={"item-header"} onClick={() => onClickHandler(folder.id)}>
-        <Icon path={activeChecker(folder.id) ? mdiFolderOpenOutline : mdiFolderOutline} size={1} />
+        <Icon className={"item-icon"} path={activeChecker(folder.id) ? mdiFolderOpenOutline : mdiFolderOutline}/>
         <p className={"item-name"}>{folder.name}</p>
       </div>
-      <FolderActions menuTarget={folder}/>
+      <FolderActionsDropdown menuTarget={folder}/>
     </li>
   )
 }
