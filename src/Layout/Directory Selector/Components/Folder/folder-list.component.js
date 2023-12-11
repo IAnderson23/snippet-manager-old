082f22290b07020fd0b03fb6a29a6fe12aaf7153
Isaiah Anderson <isaiahanderson77@icloud.com>
2023-12-10 21:24:10 -0500

    chore: update icon

--- src/Layout/Directory Selector/Components/Folder/folder-list.component.js
+++ src/Layout/Directory Selector/Components/Folder/folder-list.component.js
@@ -1,8 +1,9 @@
 import {useSetAtom} from "jotai";
+import Icon from '@mdi/react';
+import { mdiPlus } from '@mdi/js';
 
 import {menuConstructor} from "../../../../Utility/Constructors/menu-constructor.utility";
 import useAllFolders from "../../../../Custom Hooks/use-all-folders.hook";
-import {addIcon} from "../../../../Assets/svg-icons.asset";
 import {menuAtom} from "../../../../Atoms/menu-atom";
 import FolderItem from "./folder-item.component";
 
@@ -18,7 +19,9 @@ function FolderList() {
     <div className={"directory-list-container"}>
       <div className={"list-header"}>
         <h5 className={"list-name"}>Folders</h5>
-        <button onClick={onAddHandler}>{addIcon}</button>
+        <button onClick={onAddHandler}>
+          <Icon path={mdiPlus} size={1} />
+        </button>
       </div>
       <ul className={"directory-list"}>
         {allFolders.map((folder, index) => <FolderItem folder={folder} key={index} />)}
--- src/Layout/Snippet Selector/Components/add-snippet-button.component.js
+++ src/Layout/Snippet Selector/Components/add-snippet-button.component.js
@@ -1,14 +1,17 @@
 import {useSetAtom} from "jotai";
 
 import {menuConstructor} from "../../../Utility/Constructors/menu-constructor.utility";
-import {addIcon} from "../../../Assets/svg-icons.asset";
+import Icon from '@mdi/react';
+import { mdiPlus } from '@mdi/js';
 import {menuAtom} from "../../../Atoms/menu-atom";
 
 function AddSnippetButton() {
   const setMenu = useSetAtom(menuAtom);
 
   return (
-    <button id={"create-snippet-button"} onClick={() => setMenu(menuConstructor.create.snippet())}>{addIcon}</button>
+    <button id={"create-snippet-button"} onClick={() => setMenu(menuConstructor.create.snippet())}>
+      <Icon path={mdiPlus} size={1} />
+    </button>
   )
 }
 
