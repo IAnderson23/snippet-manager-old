1df296c75d26ead1b873ce7a26ff5ec24508f92a
Isaiah Anderson <isaiahanderson77@icloud.com>
2023-12-10 21:26:46 -0500

    feat: add onClick properties
    add onClick properties to menu items to handle crud function for directory folders

--- src/Layout/Directory Selector/Components/Folder/folder-actions.component.js
+++ src/Layout/Directory Selector/Components/Folder/folder-actions.component.js
@@ -1,16 +1,20 @@
 import {Icon} from "@mdi/react";
+import {useSetAtom} from "jotai";
 import {mdiDotsHorizontal} from "@mdi/js";
 
+import {menuConstructor} from "../../../../Utility/Constructors/menu-constructor.utility";
 import {Menu, MenuItem} from "../../../../Components/dropdown-menu.component";
+import {menuAtom} from "../../../../Atoms/menu-atom";
 
-//TODO: Come Back
-function FolderActionsDropdown() {
+function FolderActionsDropdown({menuTarget}) {
+  const setMenu = useSetAtom(menuAtom)
+
   const label = <Icon className={"actions-icon"} path={mdiDotsHorizontal} size={1.5} />
 
   return (
     <Menu label={label}>
-      <MenuItem label={"Rename"}/>
-      <MenuItem label={"Delete"}/>
+      <MenuItem label={"Rename"} onClick={() => setMenu(menuConstructor.edit.folder(menuTarget))}/>
+      <MenuItem label={"Delete"} onClick={() => setMenu(menuConstructor.delete.folder(menuTarget))}/>
     </Menu>
   )
 }
