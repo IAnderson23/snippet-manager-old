7942ed586da7c7be9adb5e5edd1fcf634c481516
Isaiah Anderson <isaiahanderson77@icloud.com>
2023-12-20 22:02:20 -0500

    feat: implement a dropdown menu
    implement a dropdown menu for editor header

--- src/Layout/Code Editor/Components/Header/Components/editor-button-group.component.js
+++ src/Layout/Code Editor/Components/Header/Components/editor-button-group.component.js
@@ -1,55 +1,30 @@
 import {Icon} from '@mdi/react'
-import Tippy from "@tippyjs/react"
 import 'tippy.js/themes/light-border.css';
 import { mdiDotsHorizontal } from '@mdi/js';
 import {useAtomValue, useSetAtom} from "jotai";
 
 //TODO: Come Back
 import {menuConstructor} from "../../../../../Utility/Constructors/menu-constructor.utility";
-import {fragmentAtom} from "../../../../../Atoms/Fragment/fragment.atom";
+import {Menu, MenuItem} from "../../../../../Components/dropdown-menu.component";
 import {snippetAtom} from "../../../../../Atoms/Snippet/snippet.atom";
 import {menuAtom} from "../../../../../Atoms/menu-atom";
 
 function EditorButtonGroup() {
   const setMenu = useSetAtom(menuAtom);
   const snippet = useAtomValue(snippetAtom);
-  const fragment = useAtomValue(fragmentAtom);
 
-  const content = <>
-    <button onClick={() => setMenu(menuConstructor.edit.snippet(snippet))}>Edit Snippet</button>
-    <button onClick={() => setMenu(menuConstructor.delete.snippet(snippet))}>Delete Snippet</button>
-    <button onClick={() => setMenu(menuConstructor.create.fragment())}>Add Fragment</button>
-    <button onClick={() => setMenu(menuConstructor.edit.fragment(fragment))}>Edit Fragment</button>
-    <button onClick={() => setMenu(menuConstructor.delete.fragment(fragment))}>Delete Fragment</button>
-  </>
-
-  const dropdown = (
-    <div >
-      <p >Add Snippet</p>
-      <p >Edit Fragment</p>
-      <p >Delete Fragment</p>
-    </div>
-  )
-
-
-
-  // function dropdown(attrs) {
-  //   return (
-  //     <div className={"dropdown-menu"} {...attrs}>
-  //       <p className={"dropdown-item"}>Add Snippet</p>
-  //       <p className={"dropdown-item"}>Edit Fragment</p>
-  //       <p className={"dropdown-item"}>Delete Fragment</p>
-  //     </div>
-  //   )
-  // }
+  const label = <Icon className={"menu-icon"} path={mdiDotsHorizontal} size={1}/>
 
   return (
-    <div id={"editor-buttons"}>
-      <Tippy content={dropdown} interactive={true} trigger={"click"} arrow={true} theme={"light-border"}>
-        <Icon path={mdiDotsHorizontal} size={1.5} />  
-      </Tippy>
+    <div id={"editor-button-group"}>
+      <Menu label={label} alignment={"right"}>
+        <MenuItem label={"Edit Snippet"} onClick={() => setMenu(menuConstructor.edit.snippet(snippet))}/>
+        <MenuItem label={"Add Fragment"} onClick={() => setMenu(menuConstructor.create.fragment())}/>
+        <MenuItem label={"Delete Snippet"} onClick={() => setMenu(menuConstructor.delete.snippet(snippet))}/>
+      </Menu>
     </div>
   )
+
 }
 
 export default EditorButtonGroup;
